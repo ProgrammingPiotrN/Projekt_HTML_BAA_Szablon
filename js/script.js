2f019551d1dfe39513ec6de31ede83215431e6d4
ProgrammingPiotrN <piotr_naw99@outlook.com>
2026-03-19 01:20:07 +0100

    Dodanie następnego kodu css i html

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('shrink');
+    } else {
+        navbar.classList.remove('shrink');
+    }
+});
